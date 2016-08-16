74b2cf0465a0748e5493bad3d27bd67dbd602bf2
Pedro Alvarez <palvarez89@gmail.com>
2016-08-16 21:28:06 +0100

    Move to bottle

--- wsgi.py
+++ wsgi.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python
 import os
+from bottle import route, default_app
 
-def application(environ, start_response):
+#app = bottle.Bottle()
 
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        response_body = '''<!doctype html>
-<html lang="en">
-<head>
-</head>
-<body>
-  <h1>Hello World</h1>
-</body>
-</html>'''
+@route('/')
+def index():
+    return '<strong>Hello World!</strong>'
+
+@route('/env')
+def env():
+    response_body = ['%s: %s' % (key, value)
+                for key, value in sorted(os.environ.items())]
+    response_body = '\n'.join(response_body)
     response_body = response_body.encode('utf-8')
+    return response_body
 
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body ]
-
+application=default_app()
 #
 # Below for testing only
 #
