aa82a5f759b52c2973bbe142da3c0e5fb424a551
Pedro Alvarez <palvarez89@gmail.com>
2016-08-30 03:18:04 +0200

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='BibaNotes',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      description='OpenShift BibaNotes app',
+      author='Pedro Alvarez',
+      author_email='palvarez89@gmail.com',
+      url='http://pedro.alvarezpiedehierro.com/',
      )
