5bc8170f1143ba690e9abc2b6213571c372f981c
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2019-03-31 01:33:55 +0100

    wip

---
+++ scrap.py
@@ -0,0 +1,37 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
+url = "http://web.mta.info/developers/turnstile.html"
+url = "http://www.bicibadajoz.es/estado/EstadoactualBis.asp"
+print("get url")
+response = requests.get(url)
+print("got url")
+
+soup = BeautifulSoup(response.text, "html.parser")
+print("find all trs")
+tables = soup.findAll('tr')
+print(len(tables))
+
+
+#for i in range(len(tables)):
+#    print(i)
+#    print(i)
+#    print(i)
+#    print(type(tables[i]))
+#    print(type(tables[i]))
+#    print(type(tables[i]))
+#    print(tables[i])
+
+print("deal with trs")
+for i in range(4, len(tables)):
+    if "APARCAMIENTO" in str(tables[i]):
+        #print(tables[i])
+        titulo = tables[i].findAll("td", {"class": "titulo"})
+        print(titulo)
+    else:
+        if "ESTADO" in str(tables[i]):
+            estado = tables[i].findAll('td', {'class': 'lat2'})
+            print(estado)
